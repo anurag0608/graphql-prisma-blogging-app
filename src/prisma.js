fd2e177a9c36350880c31323ce8dd7043e0ce734
Anurag Verma <anuragv.1020@gmail.com>
2020-04-13 00:53:38 +0530

    prisma mutation

--- src/prisma.js
+++ src/prisma.js
@@ -7,6 +7,26 @@ const prisma = new Prisma({
 
 // prisma.query prisma.mutation prisma.subscription prisma.exists
 // prisma.query.users("OPERATION ARG","SELECTION SET")
-prisma.query.users(null,'{id name email}').then((data)=>{
-    console.log(data)
+// prisma.query.users(null,'{id name posts{id title}}').then((data)=>{
+//     console.log(JSON.stringify(data,null,3))
+// })
+// prisma.query.comments(null,`{id text author{id name}}`).then((data)=>{
+//     console.log(JSON.stringify(data,null,3))
+// })
+prisma.mutation.createPost({
+    data:{
+        title: "My new graphql post is live!",
+        body: "this is body",
+        published: true,
+        author:{
+            connect:{
+                id:"ck8xafy0k002q0775g8p40c0t"
+            }
+        }
+    }
+},'{id title body published}').then((data)=>{
+    console.log(JSON.stringify(data,null,3))
+    return prisma.query.users(null,`{id name posts{id title}}`)
+}).then(data=>{
+    console.log(JSON.stringify(data,null,3))
 })
