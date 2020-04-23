5eb1206b306619c4e3a75781f11c3a606ea19d18
Anurag Verma <anuragv.1020@gmail.com>
2020-04-23 16:35:56 +0530

    added beforeEach(..)-->JEST

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,10 +1,51 @@
 import '@babel/polyfill/noConflict'
 import 'cross-fetch/polyfill'
 import ApolloBoost,{ gql } from 'apollo-boost'
+import prisma from '../src/prisma'
+import bcrypt from 'bcryptjs'
 const client = new ApolloBoost({
     uri:"http://localhost:3000"
 })
+beforeEach(async ()=>{
+    await prisma.mutation.deleteManyPosts()
+    await prisma.mutation.deleteManyUsers()
+    const user = await prisma.mutation.createUser({
+        data:{
+            name:"axdu",
+            email:"axdu.something@something.com",
+            age:20,
+            password: bcrypt.hashSync("asb@#$%^lnjfekl646416")
+        }
+    },`{id}`)
+    const posts = [{
+        title:"test post 1",
+        body:".......",
+        published:true,
+        author:{
+            connect:{
+                id:user.id
+            }
+        }
+    },{
+        title:"test post 2",
+        body:".......",
+        published:false,
+        author:{
+            connect:{
+                id:user.id
+            }
+        }
+    }]
+    posts.forEach(async (post)=>{
 
+        await prisma.mutation.createPost({
+            data: post
+        })
+    
+    })
+
+
+},15000) //this is the max timeout of this async
 test("Should create a new user",async ()=>{
     const createUser = gql`
         mutation{
@@ -26,4 +67,4 @@ test("Should create a new user",async ()=>{
     const response = await client.mutate({
         mutation: createUser
     })
-})
+},10000)
